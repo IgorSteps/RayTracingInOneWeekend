ed8cc256f4e41e677695a9279a6166cdd6313bf6
IgorSteps <igorstepanenko696@gmail.com>
2023-07-14 17:36:59 +0100

    added a sphere

--- main.cpp
+++ main.cpp
@@ -3,11 +3,27 @@
 #include "vec3.h"
 #include "ray.h"
 
+// Checks if the ray hits the sphere. 
+bool hitSphere(const point3& center, double radius, const ray& r) {
+	vec3 oc = r.origin() - center;
+
+	auto a = dot(r.direction(), r.direction());
+	auto b = 2.0 * dot(oc, r.direction());
+	auto c = dot(oc, oc) - radius * radius;
+
+	auto discriminant = b * b - 4 * a * c;
+	return (discriminant > 0);
+}
 
 // Linearly blends white and blue depending on the height of the y coordinate
 // after scaling the ray direction to unit length(so −1.0 < y < 1.0).
 color rayColor(const ray& r)
 {
+	if (hitSphere(point3(0, 0, -1), 0.5, r))
+	{
+		return color(1, 0, 0);
+	}
+
 	vec3 unitDir = unitVector(r.direction());
 	auto t = .5 * (unitDir.y() + 1.0);
 
