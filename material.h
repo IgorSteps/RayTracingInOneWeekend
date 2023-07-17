619016da81d19a0d6b26fdae0e453d1db2fd76c1
IgorSteps <igorstepanenko696@gmail.com>
2023-07-17 19:39:45 +0100

    dielectrics pt. 2

--- material.h
+++ material.h
@@ -60,6 +60,8 @@ public:
 public:
 	color albedo;
 	double fuzzines;
+
+
 };
 
 class dielectric : public material {
@@ -69,17 +71,39 @@ public:
 	virtual bool scatter(const ray& rIn, const hitRecord& rec, color& attenuation, ray& scattered) const override
 	{
 		attenuation = color(1.0, 1.0, 1.0);
-		double refractionRation = rec.frontFace ? (1.0 / refractionInd) : refractionInd;
+		double refractionRatio = rec.frontFace ? (1.0 / refractionInd) : refractionInd;
 
-		vec3 unit_direction = unitVector(rIn.direction());
-		vec3 refracted = refract(unit_direction, rec.normal, refractionRation);
+		vec3 unitDir = unitVector(rIn.direction());
+		double cosTheta = fmin(dot(-unitDir, rec.normal), 1.0);
+		double sinTheta = sqrt(1.0 - cosTheta * cosTheta);
 
-		scattered = ray(rec.p, refracted);
+		bool cannotRefract = refractionRatio * sinTheta > 1.0;
+		vec3 direction;
+
+		if (cannotRefract || reflectance(cosTheta, refractionRatio) > randomDouble())
+		{
+			direction = reflect(unitDir, rec.normal);
+		}
+		else
+		{
+			direction = refract(unitDir, rec.normal, refractionRatio);
+		}
+
+		scattered = ray(rec.p, direction);;
 		return true;
 	}
 
 public:
-	double refractionInd; 
+	double refractionInd;
+
+private:
+	// use Schlick's approximation for reflectance.
+	static double reflectance(double cosine, double refIdx) {
+		auto r0 = (1 - refIdx) / (1 + refIdx);
+		r0 = r0 * r0;
+		return r0 + (1 - r0) * pow((1 - cosine), 5);
+	}
+
 };
 
 #endif
